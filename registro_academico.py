be0db2ee4601dca7c414b57aa4537bc76a0ffe95
Vitória Assumpção <vitoria.assumpcao@xmarts.com>
2026-04-16 17:33:31 -0300

    Remove inicializações redundantes no construtor

--- registro_academico.py
+++ registro_academico.py
@@ -4,16 +4,12 @@ class RegistroAcademico:
         self.nome_aluno = nome_aluno
         self.numero_matricula = numero_matricula
         self.eh_bolsista = eh_bolsista
-        self.mensalidade = 0.0
-        self.desconto = 0.0
 
     def calcula_mensalidade(self, valor_base: float):
         if self.eh_bolsista == True:
-            self.mensalidade = valor_base / 2
-            print(f"Mensalidade: {self.mensalidade}")
+            print(f"Mensalidade: {valor_base / 2}")
         else:
-            self.mensalidade = valor_base
-            print(f"Mensalidade: {self.mensalidade}")
+            print(f"Mensalidade: {valor_base}")
 
     def mostra_registro(self):
         print(f"Nome: {self.nome_aluno}")
