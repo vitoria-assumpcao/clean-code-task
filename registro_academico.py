e8800941b1503c3e055c13539fd624964db290bc
Vitória Assumpção <vitoria.assumpcao@xmarts.com>
2026-04-16 17:34:35 -0300

    Adiciona getters para acesso aos dados

--- registro_academico.py
+++ registro_academico.py
@@ -1,17 +1,29 @@
 class RegistroAcademico:
 
     def __init__(self, nome_aluno: str, numero_matricula: int, eh_bolsista: bool):
-        self.nome_aluno = nome_aluno
-        self.numero_matricula = numero_matricula
-        self.eh_bolsista = eh_bolsista
+        self._nome_aluno = nome_aluno
+        self._numero_matricula = numero_matricula
+        self._eh_bolsista = eh_bolsista
+
+    @property
+    def nome_aluno(self) -> str:
+        return self._nome_aluno
+
+    @property
+    def numero_matricula(self) -> int:
+        return self._numero_matricula
+
+    @property
+    def eh_bolsista(self) -> bool:
+        return self._eh_bolsista
 
     def calcula_mensalidade(self, valor_base: float):
-        if self.eh_bolsista == True:
+        if self._eh_bolsista == True:
             print(f"Mensalidade: {valor_base / 2}")
         else:
             print(f"Mensalidade: {valor_base}")
 
     def mostra_registro(self):
-        print(f"Nome: {self.nome_aluno}")
-        print(f"Matricula: {self.numero_matricula}")
-        print(f"Bolsista: {self.eh_bolsista}")
+        print(f"Nome: {self._nome_aluno}")
+        print(f"Matricula: {self._numero_matricula}")
+        print(f"Bolsista: {self._eh_bolsista}")
